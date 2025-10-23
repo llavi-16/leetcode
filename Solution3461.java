0e8c57911d67c60d2c9ddde7997fb020828480dd
llavi-16 <pallavisingh16th@gmail.com>
2025-10-23 18:41:13 +0530

    3461

---
+++ Solution3461.java
@@ -0,0 +1,16 @@
+class Solution3461 {
+    public boolean hasSameDigits(String s) {
+        while(s.length()>2){
+            StringBuilder next = new StringBuilder();
+            for(int i = 0 ;i<s.length()-1;i++){
+                int a = s.charAt(i) -'0';
+                int b = s.charAt(i+1) - '0';
+                int sum = (a+b)%10;
+                next.append(sum);
+            }
+            s = next.toString();
+            
+        }
+        return s.charAt(0) == s.charAt(1);
+    }
+}
