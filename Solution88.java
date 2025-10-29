b3e3e289aaf9fe7c75cf31be6fb0fa92018a34f3
llavi-16 <pallavisingh16th@gmail.com>
2025-10-29 23:58:47 +0530

    88

---
+++ Solution88.java
@@ -0,0 +1,15 @@
+class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int r1 = m - 1;
+        int r2 = n - 1;
+        int w = m + n - 1;
+
+        while (r2 >= 0) {
+            if (r1 >= 0 && nums1[r1] > nums2[r2]) {
+                nums1[w--] = nums1[r1--];
+            } else {
+                nums1[w--] = nums2[r2--];
+            }
+        }
+    }
+}
