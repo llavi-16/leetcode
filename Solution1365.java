4ae1b596cb7d6f590a6348888216186ab13f754c
llavi-16 <pallavisingh16th@gmail.com>
2025-11-08 22:23:07 +0530

    1365

---
+++ Solution1365.java
@@ -0,0 +1,22 @@
+class Solution1365 {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i =0; i<nums.length;i++){
+            int count =0;
+            for(int j =0; j<nums.length;j++){
+                if(i==j){
+                    continue;
+                }
+                if(nums[i]>nums[j]){
+                    count++;   
+                }    
+            }
+            list.add(count);
+        }
+        int[] res = new int[list.size()];
+        for(int i =0; i<list.size();i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
