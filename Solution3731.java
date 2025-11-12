1b12c673109328a3d19b6ad8bf98c4578825ab72
llavi-16 <pallavisingh16th@gmail.com>
2025-11-13 00:02:20 +0530

    3731

--- Solution3731.java
+++ Solution3731.java
@@ -1,28 +1,27 @@
 class Solution3731 {
     public List<Integer> findMissingElements(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-
+        ArrayList<Integer> list = new ArrayList<>();
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-
-        // find min and max correctly
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < min) min = nums[i];
-            if (nums[i] > max) max = nums[i];
+        for(int i =0; i<nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+            }
+            if(nums[i]>max){
+                max = nums[i];
+            }
         }
-
-        // check each number between min and max
-        for (int num = min; num <= max; num++) {
+        while(min<max){
             boolean found = false;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == num) {
+            for(int i =0; i<nums.length;i++){
+                if(nums[i]==min){
                     found = true;
                     break;
                 }
             }
-            if (!found) list.add(num);
+            if(!found){list.add(min);}
+            min++;
         }
-
         return list;
     }
 }
