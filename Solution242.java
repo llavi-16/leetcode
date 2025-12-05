2bb84a2fab08dce8db0caf0e89cbd8c9aa4e3f8a
llavi-16 <pallavisingh16th@gmail.com>
2025-12-05 22:28:55 +0530

    242

---
+++ Solution242.java
@@ -0,0 +1,15 @@
+class Solution242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if (Arrays.equals(c1, c2)) {
+            return true;
+        }
+        return false;
+    }
+}
