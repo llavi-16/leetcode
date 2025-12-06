8fe14726b35cb029c12672aa0cc431bd2b1719b6
llavi-16 <pallavisingh16th@gmail.com>
2025-12-06 23:02:54 +0530

    383

---
+++ Solution383.java
@@ -0,0 +1,18 @@
+class Solution383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for(char ch: magazine.toCharArray()){
+            count[ch-'a']++;
+        }
+        for(char ch : ransomNote.toCharArray()){
+            count[ch-'a']--;
+            if(count[ch-'a']<0){
+                return false;
+            }
+        }
+        return true;
+
+
+        
+    }
+}
