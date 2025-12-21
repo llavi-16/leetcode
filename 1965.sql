4ca87c0de0c9c5e258b9ec073036706a59b0af72
llavi-16 <pallavisingh16th@gmail.com>
2025-12-21 23:42:09 +0530

    1965

---
+++ 1965.sql
@@ -0,0 +1,18 @@
+--1965 
+select e.employee_id as employee_id
+from Employees as e
+left join Salaries as s
+on e.employee_id = s.employee_id 
+where s.employee_id is null
+
+
+union
+
+
+
+select s.employee_id as employee_id
+from Salaries as s
+left join Employees  as e
+on s.employee_id = e.employee_id 
+where e.employee_id is null
+order by employee_id asc;
